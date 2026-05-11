23d9a61051f68b78fc93af68b6c1b2d8c16e31fc
William Cronin <williamcronin42@gmail.com>
2026-05-11 03:11:02 -0400

    Instructor dropdown box

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -116,7 +116,7 @@ class _LoginPageState extends State<LoginPage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.Google, size: 24),
+          Icon(Icons.g_mobiledata, size: 24),
           SizedBox(width: 12),
           Text(
             'Continue with Google',
