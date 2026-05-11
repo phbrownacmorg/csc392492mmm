f037bf98248967fa365993390a703d8fde4b8c95
Tatiana790 <tabrown002@converse.edu>
2026-05-10 22:49:17 -0400

    auth service.dart

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,132 @@
+// A collection of functions that communicate with login_page, register_page, and
+// profile_page to handle most of the database logic on behalf of those files.
+
+// Uses Flutter Mapp's example file as a starting point.
+// https://youtu.be/pioTvtt3O3I?si=fd1S52sr82R79U2q 
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+ValueNotifier<AuthService> authService = ValueNotifier(AuthService());
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => firebaseAuth.authStateChanges();
+
+  Future<UserCredential> login({
+    required String email,
+    required String password,
+  }) async {
+    return await firebaseAuth.signInWithEmailAndPassword(
+      email: email, password: password);
+  }
+
+  Future<String?> createAccount({
+    required String email,
+    required String password,
+    required String firstName,
+    required String lastName,
+    required String role,
+  }) async {
+    try {
+      final userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+      );
+      await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userCredential.user!.uid)
+        .set({
+          'uid': userCredential.user!.uid, // EVERY user has a unique UID
+          'email': email,
+          'firstName': firstName,
+          'lastName': lastName,
+          'role': role,
+          // Everything below is NOT required during registration!
+          'phone': null, // Add ability to add # to an eventual profile editor 
+          'myInstructor': null,
+          'problems': [],
+          'assignedSheets': [],
+          'completedSheets': [],
+          // TODO: Add Google account field
+          // TODO: Add Facebook account field
+        });
+
+      return null;
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  // TODO: Create new function to handle login with Google account
+
+  // TODO: Create new function to handle login with Facebook account
+
+  Future<Map<String, dynamic>?> getUserData() async {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .get();
+      return doc.data() as Map<String, dynamic>?;
+    }
+    return null;
+  }
+
+  Future<bool> doesEmailExist(String email) async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+      .collection('users')
+      .where('email', isEqualTo: email)
+      .get();
+    return snapshot.docs.isNotEmpty;
+  }
+
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
+  }
+
+
+
+  // Everything below is other functions from Flutter Mapp. We can either use them
+  // later, or delete them if we don't need them.
+
+  // Future<void> resetPassword({
+  //   required String email,
+  // }) async {
+  //   await firebaseAuth.sendPasswordResetEmail(email: email);
+  // }
+
+  // Future<void> updateUsername({
+  //   required String username,
+  // }) async {
+  //   await currentUser!.updateDisplayName(username);
+  // }
+
+  // Future<void> deleteAccount({
+  //   required String email,
+  //   required String password,
+  // }) async {
+  //   AuthCredential credential = 
+  //     EmailAuthProvider.credential(email: email, password: password);
+  //   await currentUser!.reauthenticateWithCredential(credential);
+  //   await currentUser!.delete();
+  //   await firebaseAuth.signOut();
+  // }
+
+  // Future<void> resetPasswordFromCurrentPassword({
+  //   required String currentPassword,
+  //   required String newPassword,
+  //   required String email,
+  // }) async {
+  //   AuthCredential credential = 
+  //     EmailAuthProvider.credential(email: email, password: currentPassword);
+  //   await currentUser!.reauthenticateWithCredential(credential);
+  //   await currentUser!.updatePassword(newPassword);
+  // }
+}
